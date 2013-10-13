975d762ef807fc8ccda69547e0eb163bd332e646
Camilo <Camilo@idea-PC>
2013-10-13 13:05:13 -0500

    Entity Class and persistence Database name: data Database user: root Database pass: root

--- src/com/data/entity/Account.java
+++ src/com/data/entity/Account.java
@@ -4,10 +4,46 @@
  */
 package com.data.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 /**
  *
  * @author Camilo
  */
+@Entity
+@Table(name = "account")
 public class Account {
+    @Id
+    private String numberAccount;
+    private double balance;
+    @OneToOne
+    private Profile profile;
+
+    public String getNumberAccount() {
+        return numberAccount;
+    }
+
+    public void setNumberAccount(String numberAccount) {
+        this.numberAccount = numberAccount;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
     
 }
--- src/com/data/entity/Profile.java
+++ src/com/data/entity/Profile.java
@@ -4,10 +4,59 @@
  */
 package com.data.entity;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Camilo
  */
-public class Profile {
+
+@Entity
+@Table(name = "profile")
+public class Profile implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long cedule;
+    private String name;
+    private String email;
+    private double localBalance;
+
+    public long getCedule() {
+        return cedule;
+    }
+
+    public void setCedule(long cedule) {
+        this.cedule = cedule;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getLocalBalance() {
+        return localBalance;
+    }
+
+    public void setBalance(double localBalance) {
+        this.localBalance = localBalance;
+    }   
     
 }
