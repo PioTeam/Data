b282146992e6fcdd65492f3aaa3b9564b6bb1aa8
Camilo <Camilo@idea-PC>
2013-10-12 23:34:33 -0500

    Cascade remove

---
+++ src/com/data/entity/Account.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.data.entity;
+
+/**
+ *
+ * @author Camilo
+ */
+public class Account {
+    
+}
